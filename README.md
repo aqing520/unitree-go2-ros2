60e0d7bd33a42dba0fd8a6d9ae9ca4300b1eb99f
wzy <2660828538@qq.com>
2026-03-19 19:24:40 +0800

    添加对livox的支持,重写启动脚本

--- README.md
+++ README.md
@@ -96,13 +96,13 @@ https://github.com/user-attachments/assets/bcfeec70-12c5-49b8-b7a7-da4fa9b6dea5
 
 ### 2.4 Go2 Velodyne Config Gazebo demo: Run the Gazebo environment
 ```bash
-ros2 launch go2_config gazebo_velodyne.launch.py 
+ros2 launch go2_config gazebo_livox.launch.py 
 ```
 ![Go2 Velodyne Gazebo Launch](.docs/gazebo_velodyne_launch.png)
 
 ### 2.5 Go2 Veldyne Config Walking/PointCloud demo in RVIZ: Run the gazebo along with rviz
 ```bash
-ros2 launch go2_config gazebo_velodyne.launch.py rviz:=true
+ros2 launch go2_config gazebo_livox.launch.py rviz:=true
 ```
 
 > Note: set point cloud topic to `/velodyne_points`
@@ -114,12 +114,12 @@ ros2 launch go2_config gazebo_velodyne.launch.py rviz:=true
 > NOTE: To use Laser instead of 3D Velodyne LiDAR, comment `<xacro:include filename="$(find go2_description)/xacro/velodyne.xacro"/>` and uncomment `<xacro:include filename="$(find go2_description)/xacro/laser.xacro"/>` in `robot_VLP.xacro` file located inside `robots/description/go2_description/xacro/` folder.
 
 ```bash
-ros2 launch go2_config gazebo_velodyne.launch.py 
+ros2 launch go2_config gazebo_livox.launch.py 
 ```
 
 To Run the gazebo along with rviz
 ```bash
-ros2 launch go2_config gazebo_velodyne.launch.py rviz:=true
+ros2 launch go2_config gazebo_livox.launch.py rviz:=true
 ```
 
 ## 3. Tuning gait parameters
--- src/robots/configs/go2_config/launch/gazebo_livox.launch.py
+++ src/robots/configs/go2_config/launch/gazebo_livox.launch.py
@@ -2,29 +2,39 @@ import os
 
 import launch_ros
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+)
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration
 
 
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    description_path = LaunchConfiguration("description_path")
+    base_frame = "base_link"
+
     config_pkg_share = launch_ros.substitutions.FindPackageShare(
         package="go2_config"
     ).find("go2_config")
     descr_pkg_share = launch_ros.substitutions.FindPackageShare(
         package="go2_description"
     ).find("go2_description")
-
     joints_config = os.path.join(config_pkg_share, "config/joints/joints.yaml")
+    ros_control_config = os.path.join(
+        config_pkg_share, "/config/ros_control/ros_control.yaml"
+    )
     gait_config = os.path.join(config_pkg_share, "config/gait/gait.yaml")
     links_config = os.path.join(config_pkg_share, "config/links/links.yaml")
-
-    # Use robot_VLP.xacro so Velodyne 3D LiDAR is loaded by default.
-    default_model_path = os.path.join(descr_pkg_share, "xacro/robot_VLP.xacro")
-    # Use outdoor world by default; can still be overridden via world:=...
-    default_world_path = os.path.join(config_pkg_share, "worlds/outdoor.world")
+    default_model_path = os.path.join(descr_pkg_share, "xacro/robot_Livox.xacro")
+    default_world_path = os.path.join(config_pkg_share, "worlds/default.world")
 
     declare_use_sim_time = DeclareLaunchArgument(
         "use_sim_time",
@@ -32,7 +42,7 @@ def generate_launch_description():
         description="Use simulation (Gazebo) clock if true",
     )
     declare_rviz = DeclareLaunchArgument(
-        "rviz", default_value="true", description="Launch rviz"
+        "rviz", default_value="false", description="Launch rviz"
     )
     declare_robot_name = DeclareLaunchArgument(
         "robot_name", default_value="go2", description="Robot name"
@@ -40,9 +50,15 @@ def generate_launch_description():
     declare_lite = DeclareLaunchArgument(
         "lite", default_value="false", description="Lite"
     )
+    declare_ros_control_file = DeclareLaunchArgument(
+        "ros_control_file",
+        default_value=ros_control_config,
+        description="Ros control config path",
+    )
     declare_gazebo_world = DeclareLaunchArgument(
         "world", default_value=default_world_path, description="Gazebo world name"
     )
+
     declare_gui = DeclareLaunchArgument(
         "gui", default_value="true", description="Use gui"
     )
@@ -52,7 +68,13 @@ def generate_launch_description():
     declare_world_init_heading = DeclareLaunchArgument(
         "world_init_heading", default_value="0.0"
     )
+    declare_description_path = DeclareLaunchArgument(
+        "description_path",
+        default_value=default_model_path,
+        description="Absolute path to robot urdf file",
+    )
 
+    
     bringup_ld = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
@@ -62,7 +84,7 @@ def generate_launch_description():
             )
         ),
         launch_arguments={
-            "description_path": default_model_path,
+            "description_path": LaunchConfiguration("description_path"),
             "joints_map_path": joints_config,
             "links_map_path": links_config,
             "gait_config_path": gait_config,
@@ -106,13 +128,16 @@ def generate_launch_description():
             declare_rviz,
             declare_robot_name,
             declare_lite,
+            declare_ros_control_file,
             declare_gazebo_world,
             declare_gui,
             declare_world_init_x,
             declare_world_init_y,
             declare_world_init_z,
             declare_world_init_heading,
+            declare_description_path,
             bringup_ld,
-            gazebo_ld,
+            gazebo_ld
+
         ]
     )
